2cd38d3a2fd38a20b23a96bf3998ce0e128b3181
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2026-01-04 13:35:22 +0800

    continued refactoring

--- qlogicae_cpp_core/qlogicae_cpp_core/includes/mutex_manager_configurations.hpp
+++ qlogicae_cpp_core/qlogicae_cpp_core/includes/mutex_manager_configurations.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "instance_manager.hpp"
-
 namespace QLogicaeCppCore
 {
     struct MutexManagerConfigurations
--- qlogicae_cpp_core/qlogicae_cpp_core/includes/utilities_cache.hpp
+++ qlogicae_cpp_core/qlogicae_cpp_core/includes/utilities_cache.hpp
@@ -8,6 +8,116 @@ namespace QLogicaeCppCore
     class UtilitiesCache
     {
     public:
+        /*
+        std::string relative_private_qlogicae_folder_path =
+            ".qlogicae";
+
+        std::string relative_public_qlogicae_folder_path =
+            "qlogicae";
+
+        std::string relative_application_name_folder_path =
+            "";
+
+        std::string relative_application_version_folder_path =
+            "";
+
+        std::string relative_application_environment_name_folder_path =
+            "";
+
+
+
+        std::string relative_log_folder_path =
+            "log";
+
+        std::string relative_log_date_folder_path =
+            "date";
+
+        std::string relative_log_level_folder_path =
+            "level";
+
+        std::string relative_all_log_file_path =
+            "all.log";
+
+        std::string relative_info_log_file_path =
+            "info.log";
+
+        std::string relative_debug_log_file_path =
+            "debug.log";
+
+        std::string relative_warning_log_file_path =
+            "warning.log";
+
+        std::string relative_success_log_file_path =
+            "success.log";
+
+        std::string relative_critical_log_file_path =
+            "critical.log";
+
+        std::string relative_error_log_file_path =
+            "error.log";
+
+
+
+
+
+        std::string relative_cache_folder_path =
+            "cache";
+
+        std::string relative_main_folder_path =
+            "main";
+        
+
+
+        std::string relative_assets_folder_path =
+            "assets";
+
+        std::string relative_documentation_folder_path =
+            "documentation";
+
+        std::string relative_environment_json_file_path =
+            "environment.json";
+
+        std::string relative_license_txt_file_path =
+            "LICENSE.txt";
+
+        std::string relative_application_folder_path =
+            "application";
+
+        std::string relative_application_icon_file_path =
+            "application.ico";
+
+        std::string relative_qlogicae_json_file_path =
+            "qlogicae.json";
+
+        std::string relative_configurations_folder_path =
+            "configurations";
+
+        std::string relative_development_folder_path =
+            "development";
+
+        std::string relative_debug_folder_path =
+            "debug";
+
+        std::string relative_test_folder_path =
+            "test";
+
+        std::string relative_release_folder_path =
+            "release";
+
+        std::string relative_windows_registry_software_qlogicae_folder_path =
+            "Software\\QLogicae";
+
+        std::string FULL_EXECUTABLE_folder_path;
+
+        std::string FULL_EXECUTED_folder_path;
+
+        std::string FULL_ROAMING_APPDATA_folder_path;
+
+        std::string FULL_LOCAL_APPDATA_folder_path;
+
+        std::string FULL_PROGRAMDATA_folder_path;
+        */
+
         static UtilitiesCache&
             instance;
 
