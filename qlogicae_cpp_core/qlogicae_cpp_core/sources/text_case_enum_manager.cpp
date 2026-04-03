c94d947aced4a162701e430347d491de9339e115
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2026-04-03 21:35:09 +0800

    continued partial refactoring via macros definitions within the 'qlogicae_cpp_core' project

--- qlogicae_cpp_core/qlogicae_cpp_core/sources/text_case_enum_manager.cpp
+++ qlogicae_cpp_core/qlogicae_cpp_core/sources/text_case_enum_manager.cpp
@@ -21,26 +21,12 @@ namespace
 	{
 		try
         {		
-			if
+			QLOGICAE_COR_V1__EXPLICIT__HPP_CPP__PRE_EXECUTION_GUARD_TEMPLATE
 			(
-				configurations
-					.is_runtime_execution_disabled_for_feature_handling()
-			)
-			{
-				return
-					"NONE";
-			}
-
-			boost::unique_lock<boost::mutex>
-				mutex_lock;
-			if (configurations.is_thread_safety_enabled_for_feature_handling())
-			{
-				mutex_lock =
-					boost::unique_lock<boost::mutex>
-					(
-						feature_handling_mutex_1
-					);
-			}			
+				"NONE",
+				QLOGICAE_COR_V1__BASE__HPP_CPP__MUTEX_LAYER_1,
+				QLOGICAE_COR_V1__BASE__HPP_CPP__EMPTY_EDGE_CASES
+			);		
 
 			switch (value)
 			{
@@ -113,27 +99,12 @@ namespace
 	{
 		try
         {		
-			if
+			QLOGICAE_COR_V1__EXPLICIT__HPP_CPP__PRE_EXECUTION_GUARD_TEMPLATE
 			(
-				configurations
-					.is_runtime_execution_disabled_for_feature_handling()
-			)
-			{
-				return
-					TextCase
-						::NONE;
-			}
-
-			boost::unique_lock<boost::mutex>
-				mutex_lock;
-			if (configurations.is_thread_safety_enabled_for_feature_handling())
-			{
-				mutex_lock =
-					boost::unique_lock<boost::mutex>
-					(
-						feature_handling_mutex_1
-					);
-			}			
+				TextCase::NONE,
+				QLOGICAE_COR_V1__BASE__HPP_CPP__MUTEX_LAYER_1,
+				QLOGICAE_COR_V1__BASE__HPP_CPP__EMPTY_EDGE_CASES
+			);			
 
 			if
 			(
