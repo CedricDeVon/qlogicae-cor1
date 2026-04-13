53a5a028e2b3a87011096a8facdffff47cb92112
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2026-04-13 20:30:26 +0800

    updated error messages within the 'macros/pre_build_checks.hpp' file within the 'qlogicae_cor1' project

--- qlogicae_cor/qlogicae/cor1/macros/pre_build_checks.hpp
+++ qlogicae_cor/qlogicae/cor1/macros/pre_build_checks.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #if !defined(_MSC_VER)
-#error "Error - QLogicae::Cor1 - Compilation via MSVC is required"
+#error "Error - qlogicae::cor1 - Compilation via MSVC is required"
 #endif
 
 #if _MSVC_LANG < 202002L
-#error "Error - QLogicae::Cor1 - Version C++20 or higher is required"
+#error "Error - qlogicae::cor1 - Version C++20 or higher is required"
 #endif
